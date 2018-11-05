5522b7321a50b3fe76d79c4b2d6cd46b997631ff
dawit <davucan@gmail.com>
2018-11-04 23:12:53 -0800

    add seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,14 @@
 User.create!( first_name: "derik",
-			  last_name: "thompson"
-			  email: "b@b.com",
-			  password: 'p')
+			  last_name: "thompson",
+			  email: "b@b.com")
 
 User.create!( first_name: "ciara",
-			  last_name: "norman"
-			  email: "c@c.com",
-			  password: 'p')
+			  last_name: "norman",
+			  email: "c@c.com")
 
 User.create!( first_name: "bob",
-			  last_name: "cat"
-			  email: "d@d.com",
-			  password: 'p')
+			  last_name: "cat",
+			  email: "d@d.com")
 
 Todo.create!( description: "send email to clients",
     	      alloted_time_in_hours: 2,
