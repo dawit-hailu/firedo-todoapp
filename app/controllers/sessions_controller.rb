a8bcde45353e99b53b76d616e09e76c5c0352de2
dawit <davucan@gmail.com>
2018-11-05 16:03:30 -0800

    create wellcome and sessions controller and helper

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # find by downcase email because of validation
+    user = User.find_by_email(params[:email].downcase)
+  end
+
+  def destroy
+    log_out if logged_in?
+    redirect_to '/'
+  end
+end
